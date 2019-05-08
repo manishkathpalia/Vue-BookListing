24397aa652153602ffad68e2ccb26667ff5d573f
Kathpalia <Manish.Kathpalia@FISGLOBAL.COM>
2019-05-08 18:49:43 +0530

    Completed the module

---
+++ src/components/BookList.vue
@@ -0,0 +1,27 @@
+<template> 
+
+<div><h1> {{title}} </h1></div>
+
+</template>
+
+
+<script>
+
+export default{
+    name: 'BookList',
+    data(){
+        return{
+        title:'All Books1'                
+        }
+    }
+}
+
+</script>
+
+<style>
+h1,h2{
+font-weight:normal;
+
+}
+
+</style>
